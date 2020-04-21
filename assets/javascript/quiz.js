576affa9efe7f3d9343517f4dde4367ed9a9b4e3
kdawg <mcleod.karla@gmail.com>
2020-04-20 21:57:34 -0400

    added more pages and linked some things. Need to go to bed

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,42 @@
+< !DOCTYPE html >
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Code Quiz</title>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                        <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+                        <body>
+                            <ul class="nav nav-fill">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="/highScores.html">View High Scores</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+                                    <li class="nav-item">
+                                        <a class="nav-link disabled" href="#" tabindex="-1">Time: </a>
+                                    </li>
+    </ul>
+
+                                <div class="container">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h1 class="card-title">Coding Quiz Challenge</h1>
+                                            <p class="card-text">Try to answer the following code-related questions within the time limit. Keep in
+                    mind that incorrect answers will penalize your score/time by 10 seconds!</p>
+                                            <div class="col text-center">
+                                                <a href="quiz.html" type="submit" class="btn">Start Quiz</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <script src="assets/javascript/script.js"></script>
+
+</body>
+
+</html>
