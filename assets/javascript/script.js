c8836c96e73548ef25c7861f57a57c6beeae7a2a
kdawg <mcleod.karla@gmail.com>
2020-04-22 18:46:13 -0400

    Added code for creating a timer on the homepage

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+var timeEl = document.querySelector("#time");
+
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+
+    }, 1000);
+}
+
+setTime();
