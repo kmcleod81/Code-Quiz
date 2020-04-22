d2b6861e473b608c85aa8d04dfefefa11972bd4b
kdawg <mcleod.karla@gmail.com>
2020-04-22 18:52:39 -0400

    Added psuedocode to timer javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,22 @@
+/// variable for pulling the time id from html
 var timeEl = document.querySelector("#time");
 
+// set seconds to what it starts at
 var secondsLeft = 60;
 
+//start of function
 function setTime() {
     var timerInterval = setInterval(function () {
+        //deduction of seconds from the timer
         secondsLeft--;
+        //what I want to populate to the page in textContent
         timeEl.textContent = "Time: " + secondsLeft;
 
+        //if statement saying if counter is zero stop the timer
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            sendMessage();
         }
-
+        // counting in milliseconds
     }, 1000);
 }
 
